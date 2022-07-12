9e4a1ab190d646e12b943007143f846586a0136d
Lê Minh Quân <marucube35@gmail.com>
2022-07-12 18:49:47 +0700

    feat: big update - add songs rendering - add animation for CD - add event handlers for buttons - add audios and change songs.js

---
+++ data/songs.js
@@ -0,0 +1,38 @@
+export const songs = [
+    {
+        name: 'Hạ Còn Vương Nắng',
+        singer: 'DatKaa',
+        path: '../assets/audio/Ha Con Vuong Nang.mp3',
+        image: 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/8/0/1/d801670cd8ecdb89750bdbe8de198021.jpg'
+    },
+    {
+        name: 'Wave Alpha',
+        singer: '7UPPERCUTS',
+        path: '../assets/audio/Wave Alpha.mp3',
+        image: 'https://avatar-ex-swe.nixcdn.com/song/2018/12/19/f/6/d/c/1545212456518_640.jpg'
+    },
+    {
+        name: 'No Internet',
+        singer: '7UPPERCUTS',
+        path: '../assets/audio/No Internet.mp3',
+        image: 'https://data.chiasenhac.com/data/cover/134/133537.jpg'
+    },
+    {
+        name: 'Nhật Ký Âm Thanh',
+        singer: 'Tofu',
+        path: '../assets/audio/Nhat Ky Am Thanh.mp3',
+        image: 'https://avatar-ex-swe.nixcdn.com/song/share/2022/01/26/1/f/7/6/1643168819945.jpg'
+    },
+    {
+        name: 'Ngoài Trời Có Sao',
+        singer: 'Tofu x PC',
+        path: '../assets/audio/Ngoai Troi Co Sao.mp3',
+        image: 'https://i.ytimg.com/vi/uWVQxKd0wHg/maxresdefault.jpg'
+    },
+    {
+        name: 'Hai Mươi',
+        singer: 'Mỹ Tâm',
+        path: '../assets/audio/Hai Muoi.mp3',
+        image: 'https://i.ytimg.com/vi/4bEoXjBrRoM/maxresdefault.jpg'
+    }
+]
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,185 @@
-const player = document.querySelector('.player')
-const playlist = document.querySelector('.playlist')
-playlist.style.marginTop = +player.clientHeight + 16 + 'px'
+import { songs } from '../data/songs.js'
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const cd = $('.cd')
+const cdImage = $('.cd-image')
+const player = $('.player')
+const playlist = $('.playlist')
+const heading = $('.header h2')
+const audio = $('audio')
+const playButton = $('.btn-center')
+const progress = $('.progress')
+const nextButton = $('.btn-next')
+const prevButton = $('.btn-prev')
+const shuffle = $('.btn-shuffle')
+
+const app = {
+    // -- Data --
+    currentIndex: 0,
+    isPlaying: false,
+    shuffleMode: false,
+    songs,
+
+    // -- Styles --
+    setStyles: function () {
+        playlist.style.marginTop = +player.clientHeight + 16 + 'px'
+    },
+
+    // -- Properties --
+    defineProperties: function () {
+        Object.defineProperty(this, 'currentSong', {
+            get: function () {
+                return this.songs[this.currentIndex]
+            }
+        })
+    },
+
+    // -- Events --
+    handleEvents: function () {
+        const cdWidth = cd.offsetWidth
+
+        // -- CD rotating --
+        const cdImageAnimation = cdImage.animate(
+            [{ transform: 'rotate(360deg)' }],
+            {
+                duration: 10000,
+                iterations: Infinity
+            }
+        )
+        cdImageAnimation.pause()
+
+        // -- CD image zooming handling --
+        document.addEventListener('scroll', () => {
+            const scroll = window.scrollY || document.documentElement.scrollTop
+            const newCdWidth = cdWidth - scroll
+            cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0
+            cd.style.opacity = newCdWidth / cdWidth
+        })
+
+        // -- Play button handling --
+        playButton.addEventListener('click', () => {
+            if (app.isPlaying) audio.pause()
+            else audio.play()
+        })
+
+        // -- When song is playing --
+        audio.onplay = function () {
+            app.isPlaying = true
+            playButton.classList.add('playing')
+            cdImageAnimation.play()
+        }
+
+        // -- When song is pausing --
+        audio.onpause = function () {
+            app.isPlaying = false
+            playButton.classList.remove('playing')
+            cdImageAnimation.pause()
+        }
+
+        // -- When song is played --
+        audio.onended = function () {
+            if (app.shuffleMode) app.shuffleSong()
+            else app.currentIndex += 1
+            app.loadCurrentSong()
+            audio.play()
+        }
+
+        // -- When song progress is updated --
+        audio.ontimeupdate = function () {
+            if (audio.duration) {
+                const duration = audio.duration
+                const currentTime = audio.currentTime
+                const percent = (currentTime / duration) * 100
+                progress.value = percent
+            }
+        }
+
+        // -- When seeking progress --
+        progress.onchange = function (e) {
+            if (audio.duration) {
+                const duration = audio.duration
+                const currentPos = e.target.value
+                const seekingTime = (currentPos * duration) / 100
+                audio.currentTime = seekingTime
+            }
+        }
+
+        // -- Next button --
+        nextButton.onclick = function () {
+            if (app.shuffleMode) app.shuffleSong()
+            else app.nextSong()
+            app.loadCurrentSong()
+            audio.play()
+        }
+
+        // -- Previous button --
+        prevButton.onclick = function () {
+            if (app.shuffleMode) app.shuffleSong()
+            else app.prevSong()
+            app.loadCurrentSong()
+            audio.play()
+        }
+
+        // -- Shuffle button --
+        shuffle.onclick = function (e) {
+            e.target.classList.toggle('btn--actived')
+            app.shuffleMode = !app.shuffleMode
+        }
+    },
+    // -- Functions --
+    renderSongs: function () {
+        const songElements = this.songs.map((song) => {
+            return `
+        <div class="song">
+            <div class="song-thumbnail">
+                <img
+                    src="${song.image}"
+                    alt=""
+                    class="song-img"
+                />
+            </div>
+            <div class="song-info">
+                <h3>${song.name}</h3>
+                <p>${song.singer}</p>
+            </div>
+            <div class="song-option">
+                <i class="fa-solid fa-ellipsis"></i>
+            </div>
+        </div>
+        `
+        })
+
+        playlist.innerHTML = songElements.join('')
+    },
+    loadCurrentSong: function () {
+        heading.textContent = this.currentSong.name
+        cdImage.style.backgroundImage = `url(${this.currentSong.image})`
+        audio.src = this.currentSong.path
+    },
+    shuffleSong: function () {
+        let newIndex
+        do {
+            newIndex = Math.round(Math.random() * (this.songs.length - 1))
+        } while (newIndex === this.currentIndex)
+        this.currentIndex = newIndex
+    },
+    nextSong: function () {
+        this.currentIndex = (this.currentIndex + 1) % this.songs.length
+    },
+    prevSong: function () {
+        this.currentIndex = this.currentIndex - 1
+        if (this.currentIndex < 0) this.currentIndex = this.songs.length - 1
+    },
+
+    start: function () {
+        this.setStyles()
+        this.defineProperties()
+        this.handleEvents()
+
+        this.loadCurrentSong()
+        this.renderSongs()
+    }
+}
+app.start()
